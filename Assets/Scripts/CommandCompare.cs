34f754dffb151cc7e644187863ab38cc0c9e6062
jc5201 <jch5201@naver.com>
2016-08-04 13:18:59 +0900

    sprite, InputM, CommandCompare
    sprite 임시로 추가
    inputManager Next()
    commandCompare 스크립트 생성

---
+++ Assets/Scripts/CommandCompare.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CommandCompare : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
