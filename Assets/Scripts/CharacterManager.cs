0f1e66e309141bdc9dde3a8ab5ad01d11fcfbeb8
jc5201 <jch5201@naver.com>
2016-08-08 15:22:26 +0900

    Slider-HP sync
    Slider-hp sync

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class CharacterManager : MonoBehaviour {
@@ -12,15 +13,22 @@ public class CharacterManager : MonoBehaviour {
     {
         get; set;
     }
+
+    Slider SliderCom;
+    int num=0;
     
     // Use this for initialization
 	void Start () {
         state = CharacterStates.Rest;
+        HP = 100;
+        if (gameObject.name == "Character1") num = 1;
+        else num = 2;
+        SliderCom = GameObject.Find("Slider" + num.ToString()).GetComponent<Slider>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        SliderCom.value = (float)HP / 100;
 	}
 
     public enum CharacterStates
