31dc747e4be146911e015b8014915518946ddcad
jc5201 <jch5201@naver.com>
2016-08-09 19:54:58 +0900

    EM
    bug fix

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -13,22 +13,22 @@ public class EventManager : MonoBehaviour {
         character1 = GameObject.Find("Character1").GetComponent<CharacterManager>();
         character1 = GameObject.Find("Character2").GetComponent<CharacterManager>();
     }
+    
 	
 	// Update is called once per frame
 	void Update () {
         if (position1 != character1.position)
         {
             position1 = character1.position;
-            character1.gameObject.transform.position.x = -10 + position1;
+            character1.gameObject.transform.position = new Vector3( -10 + position1, character1.gameObject.transform.position.y, character1.gameObject.transform.position.z);
         }
         if (position2 != character2.position)
         {
             position2 = character2.position;
-            character2.gameObject.transform.position.x = -10 + position2;
+            character2.gameObject.transform.position = new Vector3(-10 + position2, character2.gameObject.transform.position.y, character2.gameObject.transform.position.z);
         }
     }
 
-    void ChangeState(int code, string cmd)
     {
         // TODO: Sprite 교ㅕ체
     }
