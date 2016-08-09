ead2d2ced279f6fe193df25f4952180a8faafabd
jc5201 <jch5201@naver.com>
2016-08-09 18:13:00 +0900

    eventmanager-position
    add position

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -3,13 +3,33 @@ using System.Collections;
 
 public class EventManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    CharacterManager character1;
+    CharacterManager character2;
+    float position1=0;
+    float position2=0;
+
+    // Use this for initialization
+    void Start () {
+        character1 = GameObject.Find("Character1").GetComponent<CharacterManager>();
+        character1 = GameObject.Find("Character2").GetComponent<CharacterManager>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (position1 != character1.position)
+        {
+            position1 = character1.position;
+            character1.gameObject.transform.position.x = -10 + position1;
+        }
+        if (position2 != character2.position)
+        {
+            position2 = character2.position;
+            character2.gameObject.transform.position.x = -10 + position2;
+        }
+    }
+
+    void ChangeState(int code, string cmd)
+    {
+        // TODO: Sprite 교ㅕ체
+    }
 }
